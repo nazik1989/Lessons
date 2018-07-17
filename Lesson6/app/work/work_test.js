2b746e6e5756011c94310cfce10a8c463e11338f
Nazik <injyannazik@gmail.com>
2018-07-18 03:44:30 +0400

    Change  content

---
+++ Lesson6/app/work/work_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('myApp.work module', function() {
+
+  beforeEach(module('myApp.work'));
+
+  describe('work controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var workCtrl = $controller('WorkCtrl');
+      expect(workCtrl).toBeDefined();
+    }));
+
+  });
+});
