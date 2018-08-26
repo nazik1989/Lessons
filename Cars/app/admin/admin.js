48d3be01d03c37bcc2d871f9375db52b6d887e8f
Nazik <injyannazik@gmail.com>
2018-08-26 21:35:47 +0400

    MEAN Project

--- Cars/app/admin/admin.js
+++ Cars/app/admin/admin.js
@@ -9,6 +9,36 @@ angular.module('myApp.admin', ['ngRoute'])
         });
     }])
 
-    .controller('AdminCtrl', ['$scope','$http',function($scope,$http) {
+    .controller('AdminCtrl', ['$scope','$http','$location', function($scope,$http, $location) {
 
-}]);
+        $http.get("http://localhost:8081/api/car_list")
+            .then(function(response) {
+                $scope.cars = response.data;;
+            });
+
+                    $scope.create = function() {
+                    console.log( $scope.car);
+
+                    var formicEkacTvyalner = $scope.car;
+
+                        $http.post('http://localhost:8081/create', (formicEkacTvyalner, response) => {
+                            const postBody = formicEkacTvyalner.body;
+                            console.log(postBody);
+                        });
+/*
+                        $http.post('http://localhost:8081/create',aaa).success(function(aaa, status) {
+                            console.log('Data posted successfully');
+                        })
+*/                      function timeoutFunction() {
+                            setTimeout(function(){location.reload(); }, 3000);
+                        };
+                        timeoutFunction();
+
+
+                        };
+
+
+    }]);
+
+//https://stackoverflow.com/questions/39153424/how-to-pass-a-json-object-to-node-js-server
+//https://stackoverflow.com/questions/47706022/error-cannot-find-module-cors
--- node/server.js
+++ node/server.js
@@ -1,15 +1,20 @@
 var express = require('express');
+var app = express();
 // npm install --save express-fileupload ենք անում, որ կարողանանք օգտագործել express-fileupload մոդուլը Ֆայլ ներբեռնելու համար
 const fileUpload = require('express-fileupload');
 var MongoClient = require('mongodb').MongoClient // npm install mongodb ենք անում, որ աշխատի սա
 var dbUrl = 'mongodb://localhost:27017/AutoShop';
 var dbName = 'AutoShop';
 var collName = 'cars';
-var app = express();
-app.use(fileUpload());
-var myCarsJson = require('./cars.json');
+//var myCarsJson = require('./cars.json');
 var fs = require('fs');
 var http = require('http');
+var path = require('path');
+var bodyParser = require('body-parser');
+app.use(fileUpload());
+app.use(bodyParser.urlencoded({ extended: false })); // for parsing       application/x-www-form-urlencoded
+var cors = require('cors');
+app.use(cors());
 
 // This responds with "Hello World" on the homepage
 app.get('/', function (req, res) {
@@ -17,23 +22,45 @@ app.get('/', function (req, res) {
    res.send('Hello GET');
 })
 
+app.get('/', (request, response) =>  response.sendFile(`${__dirname}/index.html`));
+
+app.post('/create',function (req, res) {
+  //  res.setHeader('Access-Control-Allow-Origin', '*');
+  const postBody = req.body;
+  console.log(postBody);
+  console.log("text");
+
+  MongoClient.connect(dbUrl, function (err, client) {
+  if (err) throw err
+
+  var db = client.db(dbName)
+  var myobj = postBody;
+  db.collection(collName).insertOne(myobj, function(err, res) {
+    if (err) throw err
+	console.log("1 document inserted");
+    client.close();
+	})	 
+  });
+});
+
 
 // post request է ուղարկվել  file_uploadPage էջին
 app.post('/file_uploadPage', function (req, res) {
     if (!req.files)
         return res.status(400).send('No files were uploaded.');
 // Փոփոխական, որը վերցնում է ուղարկված ֆայլը
-    let SomeFile = req.files.inputfileName;
+    let CarPicture = req.files.inputCarPictureName;
 
 // Use the mv() method to place the file somewhere on your server
 // Օգտագործում ենք mv() մեթոդը մեր upload արած ֆայլը սերվերում ինչ-որ տեղ տեղադրելու համար(FolderUploadFiles պապկայի մեջ)
-    SomeFile.mv('../Cars/app/cars/carsImage/'+SomeFile.name, function(err) {
+    CarPicture.mv('../Cars/app/cars/carsImage/'+CarPicture.name, function(err) {
         if (err)
             return res.status(500).send(err);
 
         res.send('Ֆայլը գցել է carsImage պապկայի մեջ:Գնացեք կոֆե խմելու');
     });
 })
+
 // This responds a DELETE request for the /del_user page.
 app.delete('/del_user', function (req, res) {
    console.log("Got a DELETE request for /del_user");
@@ -42,18 +69,16 @@ app.delete('/del_user', function (req, res) {
 
 // This responds a GET request for the /list_user page.
 app.get('/api/car_list', function (req, res) {
-	res.setHeader('Access-Control-Allow-Origin', '*');
+	//res.setHeader('Access-Control-Allow-Origin', '*');
 	console.log("Դուք GET request ուղարկեցիք /api/car_list ին");
 	MongoClient.connect(dbUrl, function (err, client) {
   if (err) throw err
 
   var db = client.db(dbName)
-  
   db.collection(collName).find().toArray(function (err, myCars) {
     if (err) throw err
 	res.send(myCars);
-
-	})	 
+    })
   });
 })
 
@@ -64,9 +89,9 @@ app.get('/ab*cd', function(req, res) {
 })
 
 var server = app.listen(8081, function () {
-
    var host = server.address().address
    var port = server.address().port
-
    console.log("Բարև, աշխատող սերվեր http://%s:%s", host, port)
 })
+
+//https://fullstack-developer.academy/how-do-you-extract-post-data-in-node-js/
